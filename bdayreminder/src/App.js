d4d0b2fefdd6231055b4fa1b33deac6900758183
unknown <micposso@gmail.com>
2020-12-21 17:45:44 -0500

    add bootstrap components and layout

--- bdayreminder/src/App.js
+++ bdayreminder/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Button from 'react-bootstrap/Button';
+
+import List from './components/ListComponent'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="p-3">
+      <Row>
+      <Col>
+      </Col>
+      <Col>
+        <Jumbotron>
+        <h1 className="header">BirthDays this Month</h1>
+        <List />
+        <Button>Clear All</Button>
+        </Jumbotron>
+      </Col>
+      <Col>
+      </Col>
+      </Row>
+    </Container>
   );
 }
 
---
+++ bdayreminder/src/components/ListComponent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Listcomponent = () => (
+  <h2>ListComponent</h2>
+)
+
+export default Listcomponent;
