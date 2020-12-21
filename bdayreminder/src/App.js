dd67bb494bddc559dcf39c1540187addfde7e00b
unknown <micposso@gmail.com>
2020-12-21 18:09:41 -0500

    style design

--- bdayreminder/src/App.js
+++ bdayreminder/src/App.js
@@ -1,27 +1,37 @@
-import './App.css';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Button from 'react-bootstrap/Button';
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Dropdown from "react-bootstrap/Dropdown";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Button from "react-bootstrap/Button";
 
-import List from './components/ListComponent'
+import List from "./components/ListComponent";
 
 function App() {
   return (
     <Container className="p-3">
       <Row>
-      <Col>
-      </Col>
-      <Col>
-        <Jumbotron>
-        <h1 className="header">BirthDays this Month</h1>
-        <List />
-        <Button>Clear All</Button>
-        </Jumbotron>
-      </Col>
-      <Col>
-      </Col>
+        <Col></Col>
+        <Col xs={6}>
+          <Jumbotron>
+            <h1 className="header">BirthDays Calendar</h1>
+            <Dropdown>
+              <Dropdown.Toggle variant="success" id="dropdown-basic">
+                Pick Month
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+            <List />
+            <>
+            <Button variant="primary">Clear All</Button>{' '}
+            <Button variant="success">Add Birthday</Button>
+            </>
+          </Jumbotron>
+        </Col>
+        <Col></Col>
       </Row>
     </Container>
   );
--- bdayreminder/src/components/ListComponent.js
+++ bdayreminder/src/components/ListComponent.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React from "react";
+import Card from 'react-bootstrap/Card';
 
-const Listcomponent = () => (
-  <h2>ListComponent</h2>
-)
+const Listcomponent = () => {
+  return (
+    <Card className="my-2">
+      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
 
 export default Listcomponent;
