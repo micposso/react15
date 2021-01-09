b3df7d87235cec59ba86ad27afa95539c9e5cacc
unknown <micposso@gmail.com>
2021-01-09 13:57:00 -0500

    finish review component

--- reviews/src/App.js
+++ reviews/src/App.js
@@ -1,11 +1,37 @@
-import Reviews from './Reviews';
-import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+import Reviews from "./Reviews";
+import AccessAlarmIcon from "@material-ui/icons/AccessAlarm";
+
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Container from "@material-ui/core/Container";
+import Avatar from '@material-ui/core/Avatar';
+
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: "center",
+      color: theme.palette.text.secondary,
+    },
+  })
+);
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <h2>reviews project setup
-      <AccessAlarmIcon color="secondary" />
-    </h2>
+    <div className={classes.root}>
+      <Container maxWidth="sm" fixed>
+        <Grid item xs={12}>
+          <Reviews />
+        </Grid>
+      </Container>
+    </div>
   );
 }
 
--- reviews/src/Reviews.js
+++ reviews/src/Reviews.js
@@ -1,9 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+
+import Paper from "@material-ui/core/Paper";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: "center",
+      color: theme.palette.text.secondary,
+    },
+    avatar: {
+      margin: "auto",
+    },
+  })
+);
 
 const Reviews = () => {
+  const classes = useStyles();
+
   return (
-    <h2>Reviews</h2>
-  )
-}
+    <Paper elevation={3} className={classes.paper}>
+      <Avatar
+        className={classes.avatar}
+        alt="Remy Sharp"
+        src="/static/images/avatar/1.jpg"
+      />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin congue
+        imperdiet lobortis. Etiam sit amet pulvinar diam. Integer imperdiet dui
+        urna, consectetur feugiat tellus dictum nec. Nunc feugiat nec velit in
+        ultrices. Duis sed tortor cursus, laoreet nibh a, fermentum eros.
+        Praesent in eros sit amet tellus interdum suscipit. Praesent aliquam
+        vestibulum pharetra. Quisque ut maximus nisl. Nullam iaculis lectus est,
+        id lacinia odio euismod sed.
+      </p>
+      <Button variant="contained">Next</Button>
+    </Paper>
+  );
+};
 
 export default Reviews;
