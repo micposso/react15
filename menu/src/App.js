85256abad5d952864a0b9ad30bc47126741ee5df
unknown <micposso@gmail.com>
2021-01-11 22:11:31 -0500

    add categories filter

--- menu/src/App.js
+++ menu/src/App.js
@@ -7,12 +7,21 @@ function App() {
   const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState([]);
 
+  const filterItems = (category) => {
+    if(category === 'all') {
+      setMenuItems(items)
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  }
+
   return <main>
     <section className="menu section">
       <div className="title">
         <h1>Our Menu</h1>
         <div className="underline"></div>
-        <Categories />
+        <Categories filterItems={filterItems} />
         <Menu items={menuItems}/>
       </div>
     </section>
--- menu/src/Categories.js
+++ menu/src/Categories.js
@@ -1,7 +1,21 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({filterItems}) => {
+
+  console.log('from categories component', filterItems);
+
+  return <div className="bnt-container">
+    <button className="filter-btn" onClick={() => {
+      filterItems('all');
+    }}>
+      all
+    </button>
+    <button className="filter-btn" onClick={() => {
+      filterItems('breakfast');
+    }}>
+      breakfast
+    </button>
+  </div>;
 };
 
 export default Categories;
