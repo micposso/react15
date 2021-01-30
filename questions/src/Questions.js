987da5d9e12ff9057d20c5a82656f85bf06f1066
unknown <micposso@gmail.com>
2021-01-30 14:27:30 -0500

    finish component functions hide and show

--- questions/src/Questions.js
+++ questions/src/Questions.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
-const Questions = () => {
-  return <h1>hola</h1>
+
+const Questions = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(false);
+  return <article className="question">
+    <header>
+      <h4>{title}</h4>
+      <button className="btn" onClick={() => setShowInfo(!showInfo)}>{showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}</button>
+    </header>
+    {showInfo && <p>{info}</p>}
+  </article>
 }
 
 export default Questions;
