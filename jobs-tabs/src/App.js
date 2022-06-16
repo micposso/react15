d78d87f98b3748ca6190805a42008c057d6bf977
micposso <micposso@gmail.com>
2022-06-15 23:47:22 -0400

    add rect icons and css styles

--- jobs-tabs/src/App.js
+++ jobs-tabs/src/App.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from 'react-icons/fa'
+
 import "./App.css";
 
 function App() {
@@ -32,14 +34,45 @@ function App() {
   }
   const { title, dates, duties, company } = jobs[value];
   return (
-    <div>
-      <h1>{title}</h1>
-      {duties.map((duty, index) => {
-        return (
-          <div>{duty}</div>
-          )
-      })}
+    <section className="section">
+    <div className="title">
+      <h2>experience</h2>
+      <div className="underline"></div>
     </div>
+    <div className="jobs-center">
+      {/* btn container */}
+      <div className="btn-container">
+        {jobs.map((item, index) => {
+          return (
+            <button
+              key={item.id}
+              onClick={() => setValue(index)}
+              className={`job-btn ${index === value && 'active-btn'}`}
+            >
+              {item.company}
+            </button>
+          )
+        })}
+      </div>
+      {/* job info */}
+      <article className="job-info">
+        <h3>{title}</h3>
+        <h4>{company}</h4>
+        <p className="job-date">{dates}</p>
+        {duties.map((duty, index) => {
+          return (
+            <div key={index} className="job-desc">
+              <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+              <p>{duty}</p>
+            </div>
+          )
+        })}
+      </article>
+    </div>
+    <button type="button" className="btn">
+      more info
+    </button>
+  </section>
   )
 }
 
