95209112c9fdf96c2eb47b26d80519e2c88afc8a
micposso <micposso@gmail.com>
2022-07-24 07:47:11 -0400

    add timer functionality to state

--- stop-watch/src/App.js
+++ stop-watch/src/App.js
@@ -1,21 +1,34 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App() {
   // setup hooks
   const [time, setTime] = useState(0);
   const [timerOn, setTimerOn] = useState(false);
 
+  // similar to componentdidmount, componentwillmout and componentupdate, it detect a change on the state
+  useEffect(() => {
+    let interval = null;
+
+    if(timerOn) {
+      interval = setInterval(() => {
+        setTime(previousTime => previousTime + 10);
+      }, 10)
+    } else {
+      clearInterval(interval);
+    }
+
+  }, [timerOn]);
 
   return (
     <div className="App">
       <div className="timer-containter">
         <div className="time">{time}</div>
         <div className="buttons-container">
-          <button></button>
-          <button></button>
-          <button></button>
-          <button></button>
+          <button onClick={() => setTimerOn(true)}>Start</button>
+          <button onClick={() => console.log("click")}>Pause</button>
+          <button onClick={() => console.log("click")}>Resume</button>
+          <button onClick={() => console.log("click")}>Clear</button>
         </div>
       </div>
     </div>
