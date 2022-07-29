738317dd0fda7b8e5be648f66269c28617638017
micposso <micposso@gmail.com>
2022-07-29 14:12:38 -0400

    set setup files

---
+++ grocery-buddy/src/App.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [name, setName] = useState('');
+  const [list, setList] = useState([])
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(false);
+  const [alert, setAlert] = useState({});
+
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
