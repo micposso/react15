6e6570135f7a465d8ccc8ffa231a12f0314fdf52
micposso <micposso@gmail.com>
2023-03-04 23:23:37 -0500

    add counter component

--- Typescript-react/src/App.tsx
+++ Typescript-react/src/App.tsx
@@ -3,9 +3,11 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 import Heading from './components/Heading'
 import { Section } from './components/Section'
+import Counter from './components/Counter'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState<number | null>(1)
 
   return (
     <div className="App">
@@ -13,8 +15,9 @@ function App() {
       <Heading title="This is my first typescript enable component" />
       <Section>
       {/* children are not props  */}
-      This content goes inside the children and it gets passed as a react node, is not a prop
+      This content goes inside the children and it gets passed as a react node, is not a prop 
       </Section>
+      <Counter setCount={setCount}>Count is {count}</Counter>
       </>
     </div>
   )
