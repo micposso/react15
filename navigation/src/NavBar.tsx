5d5d833142cdd42dd03bfa661fb5204837234bf5
micposso <micposso@gmail.com>
2023-08-08 14:22:39 -0400

    new nav folder and nav component

---
+++ navigation/src/NavBar.tsx
@@ -0,0 +1,20 @@
+import React, { useState, userRef, useEffect } from 'react';
+
+
+const NavBar = () => {
+  return
+  <Nav>
+    <div className="nav-center">
+      <div className="nav-header">
+        <img src="" alt="" />
+      </div>
+      <div className="links-container"></div>
+      <ul className="social-icons">
+
+      </ul>
+    </div>
+  </Nav>
+
+}
+
+export default NavBar;
