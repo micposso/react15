4837afb3c0c72fad17316373d732419213d328b2
micposso <micposso@gmail.com>
2023-10-12 01:35:49 -0400

    setup navbar with tailwind

--- navigation/src/NavBar.tsx
+++ navigation/src/NavBar.tsx
@@ -1,20 +1,33 @@
-import React, { useState, userRef, useEffect } from 'react';
+
+import { IoLogoAppleAr } from "react-icons/io5";
+import { links } from "./data";
+import './App.css';
 
 
 const NavBar = () => {
-  return
-  <Nav>
-    <div className="nav-center">
-      <div className="nav-header">
-        <img src="" alt="" />
+  return (
+    <nav className="bg-gradient-to-t from-[#fbc2eb] to-[#a6c1ee] h-[60px] w-full pl-4 pr-4 flex items-center justify-between">
+      <div>
+        <IoLogoAppleAr />
       </div>
-      <div className="links-container"></div>
-      <ul className="social-icons">
+      <div className="class">
+        {links.map((items: { text: any; link: any; }) => {
+        
+          const { text, link } = items;
+          return (
+            <div key={text}>
+              <a href={link}>{text}</a>
+            </div>
+          );
+        })}
 
-      </ul>
-    </div>
-  </Nav>
+      </div>
+      <div>
+        <button className="bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#fff] hover:text-black">Sign in</button>
 
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
---
+++ navigation/src/data.js
@@ -0,0 +1,25 @@
+export const links =
+  [
+    {
+      "text": "Home",
+      "link": "/"
+    },
+    {
+      "text": "About",
+      "link": "/about"
+    },
+    {
+      "text": "Services",
+      "link": "/services"
+    },
+    {
+      "text": "Contact",
+      "link": "/contact"
+    },
+    {
+      "text": "Blog",
+      "link": "/blog"
+    }
+  ];
+
+
