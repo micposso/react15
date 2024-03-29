ce8938312d12b2ca659149ddfc9b2020ef536421
micposso <micposso@gmail.com>
2024-03-29 00:15:27 -0400

    add toggle

--- navigation/src/NavBar.tsx
+++ navigation/src/NavBar.tsx
@@ -1,18 +1,29 @@
 
+import { useState } from "react";
 import { IoLogoAppleAr } from "react-icons/io5";
 import { links } from "./data";
 import './App.css';
 
 
 const NavBar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+
   return (
     <nav className="bg-gradient-to-t from-[#fbc2eb] to-[#a6c1ee] h-[60px] w-full pl-4 pr-4 flex items-center justify-between">
       <div>
         <IoLogoAppleAr />
       </div>
+      <button
+        onClick={() => setShowLinks(!showLinks)}
+        className="px-3 py-1 bg-[#a6c1ee] text-white rounded hover:bg-blue-500"
+      >
+        {showLinks ? "Hide Links" : "Show Links"}
+      </button>
+
+{showLinks && (
+
       <div className="flex space-x-4">
-        {links.map((items: { text: any; link: any; }) => {
-        
+        {links.map((items: { text: any; link: any }) => {
           const { text, link } = items;
           return (
             <div key={text}>
@@ -25,11 +36,14 @@ const NavBar = () => {
             </div>
           );
         })}
-
       </div>
-      <div>
-        <button className="bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#fff] hover:text-black">Sign in</button>
+        
+)};
 
+      <div>
+        <button className="bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#fff] hover:text-black">
+          Sign in
+        </button>
       </div>
     </nav>
   );
