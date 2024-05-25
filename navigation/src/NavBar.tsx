5af51e9fef35ed18afd753c52dde88ecd6b8f89b
micposso <micposso@gmail.com>
2024-05-25 05:55:54 -0400

    add useRef to adjust container on size

--- navigation/src/NavBar.tsx
+++ navigation/src/NavBar.tsx
@@ -1,5 +1,8 @@
 
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
+// hook that accepts an agument and returns a mutable ref object used for persisting values between renders. No like state that causes re-rendering, useRef does not. useState returns and array with two elements, the current state value and a function that lets you update it. useRef returns a mutable ref object that persists for the full lifetime of the component.
+
+// useEffect is for side effects, it is a hook that lets you perform side effects in your function components. It is a close replacement for componentDidMount, componentDidUpdate, and componentWillUnmount. It is called after every render of the component. It is used to perform data fetching, subscriptions, or manually changing the DOM in React components.
 import { IoLogoAppleAr } from "react-icons/io5";
 import { links, social } from "./data";
 import './App.css';
@@ -7,6 +10,18 @@ import './App.css';
 
 const NavBar = () => {
   const [showLinks, setShowLinks] = useState(false);
+  const linkContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  // function to detect the size of the links container
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linkContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linkContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
 
   return (
     <nav className="bg-gradient-to-t from-[#fbc2eb] to-[#a6c1ee] h-[60px] w-full pl-4 pr-4 flex items-center justify-between">
@@ -20,26 +35,24 @@ const NavBar = () => {
         {showLinks ? "Hide Links" : "Show Links"}
       </button>
 
-{showLinks && (
-
-      <div className="flex space-x-4">
-        {links.map((items: { text: any; link: any }) => {
-          const { text, link } = items;
-          return (
-            <div key={text}>
-              <a
-                className="block hover:bg-blue-500 hover:text-white px-2 py-1 rounded-md"
-                href={link}
-              >
-                {text}
-              </a>
-            </div>
-          );
-        })}
-      </div>
-        
+      {showLinks && (
+        <div className="flex space-x-4" ref={linksRef}>
+          {links.map((items: { text: any; link: any }) => {
+            const { text, link } = items;
+            return (
+              <div key={text} ref={linkContainerRef}>
+                <a
+                  className="block hover:bg-blue-500 hover:text-white px-2 py-1 rounded-md"
+                  href={link}
+                >
+                  {text}
+                </a>
+              </div>
+            );
+          })}
+        </div>
       )}
-      
+
       <ul className="flex space-x-4">
         {social.map((items: { id: any; text: any; url: any; icon: any }) => {
           const { id, url, icon, text } = items;
@@ -57,8 +70,7 @@ const NavBar = () => {
               </a>
             </li>
           );
-        }
-        )}
+        })}
       </ul>
 
       <div>
