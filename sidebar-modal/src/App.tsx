e93f12725149e854588932870d78c372326855c9
micposso <micposso@gmail.com>
2024-06-04 23:39:00 -0400

    add react context

--- sidebar-modal/src/App.tsx
+++ sidebar-modal/src/App.tsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
+import Modal from './components/Modal'
+import SideBar from './components/SideBar'
+import Home from './components/Home'
 import './App.css'
 
-function App() {
+// set up the function to open the modal from here and pass those functions as props to each component to the function to open the modal.
+// this a problem since we will have to do a lot of prop drilling to get the function to open the modal to the component that needs it.
+// we can use the context api to solve this problem.
 
+function App() {
   return (
     <>
-
+      <Modal />
+      <SideBar />
+      <Home />
     </>
   )
 }
+
 
 export default App
